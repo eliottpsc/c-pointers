804d75ffcf5d62f075c783afb7b0eff49c9c2317
eruffin eruffin <eruffin@f2r11s6.paris.42.school>
2026-03-10 23:25:53 +0100

    fix(ex6): stop reading over the end of the array

--- ex6/ft_sort_int_tab.c
+++ ex6/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: eruffin <eruffin@learner.42.tech>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 19:23:23 by eruffin           #+#    #+#             */
-/*   Updated: 2026/03/10 20:53:54 by eruffin          ###   ########.fr       */
+/*   Updated: 2026/03/10 22:38:14 by eruffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_sort_int_tab(int *tab, int size)
 	int	tmp;
 
 	i = 0;
-	while (i < size)
+	while (i < size - 1)
 	{
 		if (tab[i] <= tab[i + 1])
 			i++;
@@ -33,11 +33,11 @@ void	ft_sort_int_tab(int *tab, int size)
 #include <stdio.h>
 int	main(void)
 {
-	int	size = 10;
+	int	size = 5;
 	//int	tab[] = {3, 4, 9, 6, 7, 1, 5, 2, 0, 8};
-	int	tab[] = {-3, 4, -9, -8, 7, 1, -9, 2, 0, -9};
+	int	tab[] = {72, 97, 8, 32, 15};
 
-	ft_sort_int_tab(tab, 10);
+	ft_sort_int_tab(tab, 5);
 	for (int i = 0; i < size; i++)
 		printf("%d", tab[i]);
 }
